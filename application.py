610e6b751974af4eabe1054b8c8c11760d06c3bf
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-07-20 13:58:17 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,40 @@
 from flask import Flask
+from flask_googlemaps import GoogleMaps
 
 app = Flask(__name__)
 
+app = Flask(__name__, template_folder=".")
+GoogleMaps(app)
+
 @app.route("/")
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('example.html', mymap=mymap, sndmap=sndmap)
 
-def hello():
-
-    return "14.12"
+if __name__ == "__main__":
+    app.run(debug=True)
