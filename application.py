35f820b97bf8139a9b460c31a718ebbb57074487
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-07-20 14:22:45 -0400

    Update application.py

--- application.py
+++ application.py
@@ -7,34 +7,9 @@ app = Flask(__name__, template_folder=".")
 GoogleMaps(app)
 
 @app.route("/")
-def mapview():
-    # creating a map in the view
-    mymap = Map(
-        identifier="view-side",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[(37.4419, -122.1419)]
-    )
-    sndmap = Map(
-        identifier="sndmap",
-        lat=37.4419,
-        lng=-122.1419,
-        markers=[
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-             'lat': 37.4419,
-             'lng': -122.1419,
-             'infobox': "<b>Hello World</b>"
-          },
-          {
-             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-             'lat': 37.4300,
-             'lng': -122.1400,
-             'infobox': "<b>Hello World from other place</b>"
-          }
-        ]
-    )
-    return render_template('example.html', mymap=mymap, sndmap=sndmap)
+def dropdown():
+    colours = ['Red', 'Blue', 'Black', 'Orange']
+    return render_template('test.html', colours=colours)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
