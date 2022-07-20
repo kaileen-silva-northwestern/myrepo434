dc5e9c3dca4d37133abeccb576837adbacac6851
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-07-20 14:29:50 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,10 +3,7 @@ from flask_googlemaps import GoogleMaps
 
 app = Flask(__name__)
 
-app = Flask(__name__, template_folder=".")
-GoogleMaps(app)
-
 @app.route("/input")
 def input():
-    cityList=db.execute("SELECT * FROM cities order by city_name")
+    cityList=["SF","TF","MF"]
     return render_template("input.html",cityList=cityList )
