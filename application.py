94660cced6349566860dcf6ee23044d351d2ca1f
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-18 14:43:24 -0400

    Update application.py

--- application.py
+++ application.py
@@ -16,12 +16,20 @@ app = Flask(__name__)
 #    </body></html>
 #    """
 
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
+
 def home():
     return """
     <html><body>
     <h2> Predict Fare Prices </h2>
         <form action="/query" method ="post">
-            <input type='text' value ="nm">
+            <label for="lat1">Latitude 1:>
+            <input type='text' id="lat1" value ="nm">
             <input type='submit' value="Submit">
     </body></html>
     """
