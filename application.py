48652d17988b53e623ee6c19953f9c3d203f65fa
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-18 14:11:55 -0400

    Update application.py

--- application.py
+++ application.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 
 ##@app.route('/hello', methods=['POST'])
 
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
+def hello_world():
+    return 'Hello World'
+if __name__ == '__main__':
+   app.run()
