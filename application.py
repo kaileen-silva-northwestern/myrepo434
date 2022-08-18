660448e00627fe3307235c4b440e4dd984cf5f1d
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-18 14:47:53 -0400

    Update application.py

--- application.py
+++ application.py
@@ -28,8 +28,8 @@ def home():
     <html><body>
     <h2> Predict Fare Prices </h2>
         <form action="/query" method ="post">
-            <label for="lat1">Latitude 1:>
-            <input type='text' id="lat1" value ="nm">
+            <label for="lat1">Latitude 1:</label>
+            <input type='text' id="lat1" name="lat1">
             <input type='submit' value="Submit">
     </body></html>
     """
