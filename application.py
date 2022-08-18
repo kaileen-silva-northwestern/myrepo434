ac1e7dc0bb12ce38922ecfc1a74804ace45a17d1
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-18 14:24:29 -0400

    Update application.py

--- application.py
+++ application.py
@@ -9,7 +9,17 @@ app = Flask(__name__)
 
 ##@app.route('/hello', methods=['POST'])
 
-def hello_world():
-    return 'Hello World'
+#def hello_world():
+#    return 'Hello World'
+
+def home():
+    return """
+    <html><body>
+    <h2> Spin yo records </h2>
+        <form action="/query">
+            <input type='submit' value="I'm Feeling Lucky">
+    </body></html>
+    """
+
 if __name__ == '__main__':
    app.run()
