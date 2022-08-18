968efc18d86c0f783b58344937c0f21ed62fb855
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-18 16:25:16 -0400

    Update application.py

--- application.py
+++ application.py
@@ -26,7 +26,7 @@ def home():
     <script type="text/JavaScript">
         function showMessage(){
         document.getElementById('display').innerHTML = 
-                    document.getElementById("lat1").value;
+                    document.getElementById("lat1id").value;
     }
     </script>
     </head>
@@ -34,7 +34,7 @@ def home():
     <h2> Predict Fare Prices </h2>
         <form action="/query" method ="post">
             <label for="lat1">Latitude 1:</label><br>
-            <input type='text' id="lat1" name="lat1"><br>
+            <input type='text' name="lat1" id="lat1id" ><br>
             <label for="long1">Longitude 1:</label><br>
             <input type='text' id="long1" name="long1"><br>
             <label for="lat2">Latitude 2:</label><br>
