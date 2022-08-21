3319d80e9b4890ebbc04bb75062929ff9c9de525
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-21 17:49:24 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,10 +4,23 @@ from haversine import Unit
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 
-def hello():
-    return "Hello World!"
+#def hello():
+#    return "Hello World!"
+
+def index():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'VALUE1':
+            pass # do something
+        elif  request.form.get('action2') == 'VALUE2':
+            pass # do something else
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('index.html', form=form)
+    
+    return render_template("index.html")
 
 #def home():
 #    return """
