83068c82f445463553e3598688720f4745cd47b8
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-21 19:38:52 -0400

    Update application.py

--- application.py
+++ application.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 #def hello():
 #    return "Hello World!"
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/shortenurl')
-def shortenurl():
-    return render_template('shortenurl.html', shortcode=request.args['shortcode'])
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form
+       last_name = request.form.get("lname")
+       return "Your name is "+first_name + last_name
+    return render_template("form.html")
  
 
 #def home():
