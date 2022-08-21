f7e8dcb325e0234bd1e25331a75f934dc5008c63
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-21 19:18:18 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,22 +4,17 @@ from haversine import Unit
 
 app = Flask(__name__)
 
-#@app.route("/", methods=['POST', 'GET'])
-
 #def hello():
 #    return "Hello World!"
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
- 
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/shortenurl')
+def shortenurl():
+    return render_template('shortenurl.html', shortcode=request.args['shortcode'])
  
 
 #def home():
