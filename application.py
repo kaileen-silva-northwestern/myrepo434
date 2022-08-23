e445e05a5fb79ebfcf3f57fd3a5ba99e863ddb95
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-23 17:44:02 -0400

    Update application.py

--- application.py
+++ application.py
@@ -9,39 +9,42 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def home():
-    return """
-    <html>
-    <head>
-    <title>HTML JavaScript output on same page</title>
-    <script type="text/JavaScript">
-        function showMessage(){
-            var lat1coor = document.getElementById("lat1id").value;
-            var long1coor = document.getElementById("long1id").value;
-            var lat2coor = document.getElementById("lat2id").value;
-            var long2coor = document.getElementById("long2id").value;            
-            var message = haversine.haversine((lat1coor,long1coor),(lat2coor,long2coor),unit=Unit.MILES);
-            display_message.innerHTML= message;
-        }
-    </script>
+def homepage():
+    return render_template("home.html")
+
+#def home():
+#    return """
+#    <html>
+#    <head>
+#    <title>HTML JavaScript output on same page</title>
+#    <script type="text/JavaScript">
+#        function showMessage(){
+#            var lat1coor = document.getElementById("lat1id").value;
+#            var long1coor = document.getElementById("long1id").value;
+#            var lat2coor = document.getElementById("lat2id").value;
+#            var long2coor = document.getElementById("long2id").value;            
+#            var message = haversine.haversine((lat1coor,long1coor),(lat2coor,long2coor),unit=Unit.MILES);
+#            display_message.innerHTML= message;
+#        }
+#    </script>
       
-    </head>
-    <body>
-    <h2> Predict Fare Prices </h2>
-        <form action="/query" method ="post">
-            <label for="lat1">Latitude 1:</label><br>
-            <input type='text' name="lat1" id="lat1id" ><br>
-            <label for="long1">Longitude 1:</label><br>
-            <input type='text' name="long1" id="long1id"><br>
-            <label for="lat2">Latitude 2:</label><br>
-            <input type='text' name="lat2" id="lat2id" ><br>
-            <label for="long2">Longitude 2:</label><br>
-            <input type='text' name="long2" id="long2id"><br>
-            <input type="button" onclick="showMessage()" value="submit" />
-          </form>
-          <p> Distance between points: <span id = "display_message"></span> </p>
-    </body></html>
-    """
+#    </head>
+#    <body>
+#    <h2> Predict Fare Prices </h2>
+#        <form action="/query" method ="post">
+#            <label for="lat1">Latitude 1:</label><br>
+#            <input type='text' name="lat1" id="lat1id" ><br>
+#            <label for="long1">Longitude 1:</label><br>
+#            <input type='text' name="long1" id="long1id"><br>
+#            <label for="lat2">Latitude 2:</label><br>
+#            <input type='text' name="lat2" id="lat2id" ><br>
+#            <label for="long2">Longitude 2:</label><br>
+#            <input type='text' name="long2" id="long2id"><br>
+#            <input type="button" onclick="showMessage()" value="submit" />
+#          </form>
+#          <p> Distance between points: <span id = "display_message"></span> </p>
+#    </body></html>
+#    """
 
 if __name__ == '__main__':
    app.run()
