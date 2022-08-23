8e2c0d305b39f5a0a5c6a06b984204252185046d
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-23 18:29:18 -0400

    Update application.py

--- application.py
+++ application.py
@@ -12,6 +12,19 @@ app = Flask(__name__)
 def homepage():
     return render_template("home.html")
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+
 #def home():
 #    return """
 #    <html>
