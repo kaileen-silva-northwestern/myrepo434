7d58c6d2f5c7dac9c21cb1c49828ce4fe2590bea
kaileen-silva-northwestern <102634971+kaileen-silva-northwestern@users.noreply.github.com>
2022-08-23 20:27:35 -0400

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def homepage():
         return render_template("home.html")
                                  
